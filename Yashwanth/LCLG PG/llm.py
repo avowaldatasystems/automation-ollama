07137ba99d875da3e3752e0d780ab118000b742b
yashwanthrt <121876868+yashwanthrt@users.noreply.github.com>
2026-05-25 17:16:14 +0530

    Delete Yashwanth/LCLG PG directory

--- Yashwanth/LCLG PG/llm.py
+++
@@ -1,6 +0,0 @@
-from langchain_ollama import ChatOllama
-
-llm = ChatOllama(
-    model="llama3.2:3b",
-    temperature=0
-)
--- Yashwanth/LCLG PG/state.py
+++
@@ -1,9 +0,0 @@
-from typing_extensions import TypedDict
-from typing import List
-
-
-class AgentState(TypedDict):
-
-    question: str
-    response: str
-    chat_history: List[dict]
