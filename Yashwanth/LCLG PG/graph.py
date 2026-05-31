a266c267ca7d3fe234299df4cc1b6b1cd9ac9323
Yashwanth <yashwanthrt213@gmail.com>
2026-05-31 13:25:23 +0530

    checkin

--- Yashwanth/LCLG PG/graph.py
+++ Yashwanth/LCLG PG/graph.py
@@ -13,7 +13,7 @@ from db import engine
 from llm import llm
 from state import AgentState
 
-BASE_URL = "http://127.0.0.1:8001"
+BASE_URL = "http://127.0.0.1:8000"
 
 
 # ==================================================
